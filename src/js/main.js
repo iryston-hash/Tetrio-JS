482b69087bddf119eefa2c577b6bd29423bb9d9b
Arman B <81931943+iryston-hash@users.noreply.github.com>
2022-03-16 23:22:04 +0200

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('#button__start')
+    const width = 10
+  
+
+    //The bricks
+    const brickL = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+      ]
+    
+    const brickZ = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+      ]
+    
+    const brickT = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+      ]
+    
+    const brickB = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+      ]
+    
+    const brickI = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+      ]
+
+    const bricks = [brickL, brickB, brickI, brickZ, brickT]
+    
+    let currentPosition = 4
+    let current = bricks[0][0] 
+    
+    function draw() {
+        current.forEach(index => {
+          squares[currentPosition + index].classList.add('bricks')  
+        })
+    }
+    draw()
+
+})
